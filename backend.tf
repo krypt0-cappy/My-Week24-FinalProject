b51ec13f5baf6f8890262b6f0fbf1d26bb004bfa
= <=>
2022-10-21 13:05:12 -0400

    changes

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 #---root/backend.tf---
 
-terraform {
-  required_version = ">=0.12.0"
-  backend "s3" {
-    bucket = "my-week24-finalproject-codepipeline-bucket"
-    region = "us-east-1"
-    key    = "state/terraform.tfstate"
-  }
-}
+# terraform {
+#   required_version = ">=0.12.0"
+#   backend "s3" {
+#     bucket = "my-week24-finalproject-codepipeline-bucket"
+#     region = "us-east-1"
+#     key    = "state/terraform.tfstate"
+#   }
+# }
